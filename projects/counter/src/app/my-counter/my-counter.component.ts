57952d53a05229478a4c446ec047e1ce311633f4
Nebojsa Sretenovic <nebojsa.sretenovic96@gmail.com>
2023-11-12 14:08:46 +0100

    Implemented counter - first ngrx example

---
+++ projects/counter/src/app/my-counter/my-counter.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { decrement, increment, reset } from '../counter.actions';
+
+@Component({
+  selector: 'app-my-counter',
+  templateUrl: './my-counter.component.html',
+  styleUrls: ['./my-counter.component.scss']
+})
+export class MyCounterComponent {
+
+  count$: Observable<number> | undefined;
+
+  constructor(private store: Store<{ count: number}>) {
+    this.count$ = this.store.select('count');
+  }
+ 
+  increment() {
+    this.store.dispatch(increment());
+  }
+ 
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+ 
+  reset() {
+    this.store.dispatch(reset());
+  }
+}
